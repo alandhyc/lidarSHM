d403e237d9d2db7408ef0bec52a956e4f2f00343
Aland Chan <alandchan798@gmail.com>
2025-04-14 19:39:14 +0100

    change capitalised Z in roughness_metrics_f()

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,6 +1,5 @@
 #' LiDAR Metrics Helper Functions
 #'
-#' @return Lists to be used in pixel_metrics() and voxel_metrics()
 #' @export
 
 #Quantile 99th function
@@ -9,6 +8,7 @@ q99_f<-function(z){
   list(p_99 = quantile(z,probs = 0.99))
 }
 
+#' @export
 #Vegetation cover function
 
 cov_f<-function(z,cov_grid){
@@ -17,18 +17,19 @@ cov_f<-function(z,cov_grid){
   list(Cov = area)
 }
 
+#' @export
 #Roughness metrics function
 
 roughness_metrics_f<-function(z,shannon_cut){
 
   #Coefficient of variation
-  CV <- sd(Z, na.rm = TRUE) / mean(Z, na.rm = TRUE)
+  CV <- sd(z, na.rm = TRUE) / mean(z, na.rm = TRUE)
 
   #Roughness according to Herrero-Huerta et al. (2020)
-  roughness <- IQR(Z, na.rm = TRUE)^(median(Z, na.rm = TRUE))
+  roughness <- IQR(z, na.rm = TRUE)^(median(z, na.rm = TRUE))
 
   #Shannon according to Davison et al. (2023)
-  z_cut <- cut(Z, shannon_cut)
+  z_cut <- cut(z, shannon_cut)
   z_cut <- na.omit(z_cut)
   prop <- as.numeric(table(z_cut)) / length(z_cut)
   prop <- prop[prop > 0]
@@ -41,8 +42,11 @@ roughness_metrics_f<-function(z,shannon_cut){
   )
 }
 
+#' @export
 #Note voxel area function
 
 vox_f<-function(vox_res){
   list(vol = vox_res^3)
 }
+
+#' @return Lists to be used in pixel_metrics() and voxel_metrics()
