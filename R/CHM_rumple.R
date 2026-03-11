13cb680adcbd83feacd3af3c44e54b6f4d3b6ac4
alandhyc <66983869+alandhyc@users.noreply.github.com>
2026-03-11 05:03:11 +0000

    Update and rename CHM_rumple_norm.R to CHM_rumple.R

--- R/CHM_rumple.R
+++ R/CHM_rumple.R
@@ -8,17 +8,17 @@
 #' @import tools
 #' @import geometry
 
-CHM_rumple_norm<-function(chm_vals, chm_r){
+CHM_rumple<-function(chm_vals, chm_r){
 
   if (length(chm_vals) < 3) return(NA)
 
-  # Normalize by CHMmax (99.9th percentile)
-
-  chm_max <- quantile(chm_vals, probs = 0.999, na.rm = TRUE)
-
-  if (chm_max <= 0 || is.na(chm_max)) return(NA)
-
-  chm_norm <- chm_r / chm_max
+  ## Normalize by CHMmax (99.9th percentile)
+  #
+  #chm_max <- quantile(chm_vals, probs = 0.999, na.rm = TRUE)
+  #
+  #if (chm_max <= 0 || is.na(chm_max)) return(NA)
+  #
+  #chm_norm <- chm_r / chm_max
 
   #Calculate a slope raster
 
